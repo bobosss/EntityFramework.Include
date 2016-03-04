4239c06473b6785d53241a56304160ddc605566f
Yuki Sakai <deyu17note@gmail.com>
2016-03-05 08:59:36 +0900

    Add DbContextHelper to retrive DbContext from QueryPrivider

---
+++ EntityFramework.Include/Internal/DbContextHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Include.Internal
+{
+    internal class DbContextHelper
+    {
+        internal static DbContext GetDbContext(IQueryProvider provider)
+        {
+            var internalContext =
+                provider.GetType().GetProperty("InternalContext");
+            var owner = internalContext?.PropertyType.GetProperty("Owner");
+
+            return (DbContext)owner?.GetValue(internalContext.GetValue(provider));
+        }
+    }
+}
